eed4daf003d4094de7615648e6fcf55922039dcb
Nunoduarte1 <gomesnuno10@gmail.com>
2024-04-20 01:50:27 +0100

    dictionary to csv
    Co-Authored-By: Descatres <73725403+Descatres@users.noreply.github.com>
    Co-Authored-By: ThankTiago <102809390+ThankTiago@users.noreply.github.com>

--- backend/webscraper.py
+++ backend/webscraper.py
@@ -3,9 +3,12 @@ from selenium.webdriver.common.by import By
 # from webdriver_manager.chrome import ChromeDriverManager 
 # from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+from unidecode import unidecode
+import csv
 
 title = [] 
 data = []
+abstract =[]
 urls = ["3M",
     "American Express",
     "Amgen",
@@ -40,15 +43,34 @@ urls = ["3M",
 def main():
     counter = 1
     for url in urls:
-        if (counter == 10): break
+        if (counter == 5): break
 
         page_url = f'https://www.nytimes.com/search?dropmab=false&endDate=2016-12-31&query={url}&sections=Technology%7Cnyt%3A%2F%2Fsection%2F4224240f-b1ab-50bd-881f-782d6a3bc527&sort=best&startDate=2006-01-01'
         driver = webdriver.Chrome()
         driver.get(page_url)
         title.append(driver.find_element(By.CSS_SELECTOR,"h4.css-2fgx4k").text)
         data.append(driver.find_element(By.CLASS_NAME,"css-17ubb9w").text)
+        abstract.append(driver.find_element(By.CLASS_NAME,"css-16nhkrn").text)
         counter +=1
-    print(data)
-    print(title)
+    #print(data)
+    #print(title)
+    #print(abstract)
+
+    dict = {}
+    for i in range(len(data)):
+            dict.update({title[i]:[data[i],abstract[i]]})
+    
+    def remove_accents(text):
+        return unidecode(text)
+
+    print(dict.items())
+    with open('news.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Title', 'Date', 'Abstract'])
+        for title1, title2 in dict.items():
+            title_without_accents = remove_accents(title1)
+            description_without_accents = remove_accents(title2[0])
+            abstract_without_accents = remove_accents(title2[1])
+            writer.writerow([title_without_accents, description_without_accents, abstract_without_accents]) 
 if __name__ == "__main__":
     main()
